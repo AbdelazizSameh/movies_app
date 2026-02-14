3c0e04049bdbc48fbad273aea4a0fccc6f96664a
AbdelazizSameh <abdelzizsameh364@gmail.com>
2026-02-14 11:18:36 +0200

    add inter and roboto font family and create app font to access these fonts

--- lib/Features/splash/presentation/views/widgets/onboarding_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/onboarding_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_fonts.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: const Text(
+        "hello",
+        style: TextStyle(
+          fontFamily: AppFonts.fontInter,
+          color: Colors.white,
+          fontSize: 40,
+        ),
+      ),
+    );
   }
 }
---
+++ lib/core/utils/app_fonts.dart
@@ -0,0 +1,4 @@
+abstract class AppFonts {
+  static const String fontInter = 'Inter';
+  static const String fontRoboto = 'Roboto';
+}
